c7b55fb1878e90aa4607e4ba2b7c0491204ae02c
Aarush <78714918+AarushAce@users.noreply.github.com>
2026-02-20 13:48:47 -0500

    The nodes disappear once the key is pressed

--- hackathon/Global/GlobalStrings.gd
+++ hackathon/Global/GlobalStrings.gd
@@ -1,102 +1,52 @@
 extends Node
+
 var BString: Path2D
 var EString: Path2D
 var AString: Path2D
 var DString: Path2D
 var GString: Path2D
-var active_notes: Array = []
 
-func createBnote():
-	var path = PathFollow2D.new()
-	var area = Area2D.new()
-	var hitbox = CollisionShape2D.new()
-	var icon = Sprite2D.new()
-	
-	BString.add_child(path)
+var active_notes: Array[PathFollow2D] = []
+
+func _make_note(parent_path: Path2D, texture_path: String, action_name: String, start_progress: float) -> void:
+	var path := PathFollow2D.new()
+	path.loop = false
+	path.progress = start_progress
+
+	var area := Area2D.new()
+	area.monitoring = true
+	area.add_to_group("notes")            # IMPORTANT
+
+	var hitbox := CollisionShape2D.new()
+	var shape := RectangleShape2D.new()
+	shape.size = Vector2(60, 60)          # adjust to match your icon size
+	hitbox.shape = shape
+
+	var icon := Sprite2D.new()
+	icon.texture = load(texture_path)
+	icon.rotation = 7.0 * PI / 6.0
+
+	# store required action on the Area so the bar can check it
+	area.set_meta("action_name", action_name)
+
+	parent_path.add_child(path)
 	path.add_child(area)
 	area.add_child(hitbox)
 	area.add_child(icon)
-	
-	icon.texture = load("res://Icons/B_Icon.PNG")
-	icon.rotate(7*PI/6)
-	path.progress = 86
-	hitbox.shape = RectangleShape2D.new()
-	hitbox.scale = Vector2(3,3)
-	active_notes.append(path)
-	path.loop = false
 
-func createEnote():
-	var path = PathFollow2D.new()
-	var area = Area2D.new()
-	var hitbox = CollisionShape2D.new()
-	var icon = Sprite2D.new()
-	
-	EString.add_child(path)
-	path.add_child(area)
-	area.add_child(hitbox)
-	area.add_child(icon)
-	
-	icon.texture = load("res://Icons/E_Icon.PNG")
-	icon.rotate(7*PI/6)
-	path.progress = 45
-	hitbox.shape = RectangleShape2D.new()
-	hitbox.scale = Vector2(3,3)
 	active_notes.append(path)
-	path.loop = false
 
-func createAnote():
-	var path = PathFollow2D.new()
-	var area = Area2D.new()
-	var hitbox = CollisionShape2D.new()
-	var icon = Sprite2D.new()
-	
-	AString.add_child(path)
-	path.add_child(area)
-	area.add_child(hitbox)
-	area.add_child(icon)
-	
-	icon.texture = load("res://Icons/A_Icon.PNG")
-	icon.rotate(7*PI/6)
-	path.progress = 0
-	hitbox.shape = RectangleShape2D.new()
-	hitbox.scale = Vector2(3,3)
-	active_notes.append(path)
-	path.loop = false
+func createBnote() -> void:
+	_make_note(BString, "res://Icons/B_Icon.PNG", "B String", 86)
 
-func createDnote():
-	var path = PathFollow2D.new()
-	var area = Area2D.new()
-	var hitbox = CollisionShape2D.new()
-	var icon = Sprite2D.new()
-	
-	DString.add_child(path)
-	path.add_child(area)
-	area.add_child(hitbox)
-	area.add_child(icon)
-	
-	icon.texture = load("res://Icons/D_Icon.PNG")
-	icon.rotate(7*PI/6)
-	path.progress = 21
-	hitbox.shape = RectangleShape2D.new()
-	hitbox.scale = Vector2(3,3)
-	active_notes.append(path)
-	path.loop = false
+func createEnote() -> void:
+	_make_note(EString, "res://Icons/E_Icon.PNG", "E String", 45)
 
-func createGnote():
-	var path = PathFollow2D.new()
-	var area = Area2D.new()
-	var hitbox = CollisionShape2D.new()
-	var icon = Sprite2D.new()
-	
-	GString.add_child(path)
-	path.add_child(area)
-	area.add_child(hitbox)
-	area.add_child(icon)
-	
-	icon.texture = load("res://Icons/G_Icon.PNG")
-	icon.rotate(7*PI/6)
-	path.progress = 160
-	hitbox.shape = RectangleShape2D.new()
-	hitbox.scale = Vector2(3,3)
-	active_notes.append(path)
-	path.loop = false
+func createAnote() -> void:
+	_make_note(AString, "res://Icons/A_Icon.PNG", "A String", 0)
+
+func createDnote() -> void:
+	_make_note(DString, "res://Icons/D_Icon.PNG", "D String", 21)
+
+func createGnote() -> void:
+	_make_note(GString, "res://Icons/G_Icon.PNG", "G String", 160)
---
+++ hackathon/Levels/Level 1/hit_bar_a.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+@export var sfx: AudioStreamPlayer
+var note_in_bar: Area2D = null
+
+func _ready() -> void:
+	monitoring = true
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+
+func _process(_delta: float) -> void:
+	if note_in_bar == null:
+		return
+
+	var action_name := str(note_in_bar.get_meta("action_name", ""))
+	if action_name != "" and Input.is_action_just_pressed(action_name):
+		if sfx:
+			sfx.play()
+
+		var note_path := note_in_bar.get_parent() as PathFollow2D
+		if note_path:
+			GlobalStrings.active_notes.erase(note_path)
+			note_path.queue_free()
+
+		note_in_bar = null
+
+func _on_area_entered(a: Area2D) -> void:
+	if a.is_in_group("notes"):
+		note_in_bar = a
+
+func _on_area_exited(a: Area2D) -> void:
+	if a == note_in_bar:
+		note_in_bar = null
